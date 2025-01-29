6e523e796dce2f5ce15b0badf5b0385119c4b546
Trikhalkin.A <tank-20@list.ru>
2025-01-29 13:29:37 +0300

    First commit

--- src/widget.py
+++ src/widget.py
@@ -1,19 +1,20 @@
 def masc_account_card(number: str) -> str:
     """Функция принимает на вход номер карты и счета и возвращает их маску, номер карты и номер счета замаскирован."""
     card_number = ""
+    name_card = ''
     if "Счёт" in number or "Счет" in number:
         score_number = number[5:]
-        number_mask = "**" + score_number[2:]
+        number_mask = number[0:4] + " " + "**" + score_number[-4:]
         return number_mask
     else:
         for i in range(len(number)):
             if number[i].isalpha() or number[i] == " ":
-                pass
+                name_card += number[i]
             else:
                 card_number += number[i]
 
         correct_number = card_number[0:7] + card_number[7:14] + card_number[14:19]
-        number_mask = correct_number[0:4] + " " + correct_number[4:6] + "**" + " " + "****" + " " + correct_number[12:16]
+        number_mask = name_card + " " + correct_number[0:4] + " " + correct_number[4:6] + "**" + " " + "****" + " " + correct_number[12:16]
 
         return number_mask
 
