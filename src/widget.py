98a091b20de0d11900cef98ee07295144ad610ec
Trikhalkin.A <tank-20@list.ru>
2025-01-29 13:30:42 +0300

    First commit

--- src/widget.py
+++ src/widget.py
@@ -1,7 +1,7 @@
 def masc_account_card(number: str) -> str:
     """Функция принимает на вход номер карты и счета и возвращает их маску, номер карты и номер счета замаскирован."""
     card_number = ""
-    name_card = ''
+    name_card = ""
     if "Счёт" in number or "Счет" in number:
         score_number = number[5:]
         number_mask = number[0:4] + " " + "**" + score_number[-4:]
@@ -14,7 +14,18 @@ def masc_account_card(number: str) -> str:
                 card_number += number[i]
 
         correct_number = card_number[0:7] + card_number[7:14] + card_number[14:19]
-        number_mask = name_card + " " + correct_number[0:4] + " " + correct_number[4:6] + "**" + " " + "****" + " " + correct_number[12:16]
+        number_mask = (
+            name_card
+            + " "
+            + correct_number[0:4]
+            + " "
+            + correct_number[4:6]
+            + "**"
+            + " "
+            + "****"
+            + " "
+            + correct_number[12:16]
+        )
 
         return number_mask
 
@@ -23,5 +34,7 @@ def get_date(date: str) -> str:
     """Функция принимает на вход строку и отдает корректный результат в формате 11.07.2018."""
     correct_date = date[8:10] + "." + date[5:7] + "." + date[0:4]
     return correct_date
+
+
 n = input()
 print(masc_account_card(n))
