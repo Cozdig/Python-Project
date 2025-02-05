2e3a51e9949d64dcbed76b7c233e4e08bab126d7
Trikhalkin.A <tank-20@list.ru>
2025-02-05 11:30:22 +0300

    homework done

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,5 @@
 def filter_by_state(values: list, state: str = "EXECUTED" ) -> list:
+    """Функция принимает список операций и фильтрует их по второму аргументу"""
     filtered_values = []
     for i in values:
         if i.get("state") == state:
@@ -6,6 +7,7 @@ def filter_by_state(values: list, state: str = "EXECUTED" ) -> list:
     return filtered_values
 
 def sort_by_date(values: list, setting: bool = True) -> list:
+    """Функция принимает список операций и сортирет их по дате операции"""
     sorted_values = sorted(values, key=lambda value: value["date"], reverse=setting)
     return sorted_values
 
