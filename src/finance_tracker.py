22364e634510e730e5157e53170c87fb6448e5fe
Cozdig <tank-20@list.ru>
2025-03-26 15:53:13 +0300

    HW fix

--- src/finance_tracker.py
+++ src/finance_tracker.py
@@ -1,22 +1,30 @@
 import csv
+
 import pandas as pd
+
 
 def transactions(file):
     """Эта функция читает csv файл и выводит результат в консоль."""
     try:
-        with open(file) as csv_f:
+        with open(file, encoding="utf-8") as csv_f:
             reader = csv.DictReader(csv_f, delimiter=";")
             operations = [row for row in reader]
             return operations
     except Exception:
         return "Файл не найден"
 
+
 def transactions_excel(file):
     """Эта функция читает xlsx файл и выводит результат в консоль."""
     try:
-        with open(file, encoding="utf-8") as xlsx_f:
-            df = pd.read_excel(xlsx_f)
-            operations = df.to_dict(orient="records")
-            return operations
-    except Exception:
+        df = pd.read_excel(file)
+        operations = df.to_dict(orient="records")
+        return operations
+    except FileNotFoundError:
         return "Файл не найден"
+    except Exception as e:
+        return f"Ошибка: {e}"
+
+
+print(transactions("..\\data\\transactions.csv"))
+print(transactions_excel("..\\data\\transactions_excel.xlsx"))
